cb013e7e11c7a69c03eeb1d49ef533432ae6fa45
shamim-al-mamun <shamimalmamun.aiub@gmail.com>
2022-08-06 23:02:13 +0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+
+const connection = require("./database/MongoDB");
+const post = require("./routes/posts.routes");
+const profile = require("./routes/profile.routes");
+const contact = require("./routes/contact.routes");
+const subscription = require("./routes/subscription.routes");
+const user = require("./routes/user.routes");
+
+const app = express();
+app.use(express.json());
+// app.use(cors());
+// enables cors
+app.use(
+  cors({
+    allowedHeaders: ["sessionId", "Content-Type", "Authorization"],
+    exposedHeaders: ["sessionId", "Authorization"],
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+  })
+);
+
+//Database connection
+connection();
+//application routes
+app.use("/api/posts", post);
+app.use("/api/profile", profile);
+app.use("/api/contact", contact);
+app.use("/api/subscription", subscription);
+app.use("/api/user", user);
+
+// --------------------------deployment------------------------------
+// __dirname = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/build")));
+// }
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
+
+// --------------------------deployment------------------------------
+
+//Server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
